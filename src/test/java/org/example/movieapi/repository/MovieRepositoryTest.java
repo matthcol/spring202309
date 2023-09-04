68afba1aa28cf0ddd078e8936ea93decea5bd295
matthcol <matthias.colin@gmail.com>
2023-09-04 14:40:51 +0200

    save movie + director + actors

--- src/test/java/org/example/movieapi/repository/MovieRepositoryTest.java
+++ src/test/java/org/example/movieapi/repository/MovieRepositoryTest.java
@@ -1,15 +1,23 @@
 package org.example.movieapi.repository;
 
+import jakarta.persistence.EntityManager;
 import org.example.movieapi.entity.Movie;
+import org.example.movieapi.entity.Person;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace.NONE;
 
+// @Rollback(value = false)
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = NONE)
 @ActiveProfiles("testu")
@@ -17,6 +25,40 @@ class MovieRepositoryTest {
 
     @Autowired // DI
     MovieRepository movieRepository;
+
+    @Autowired
+    // EntityManager entityManager;
+    TestEntityManager entityManager;
+
+    // common data
+    List<Person> persons;
+    Movie movieOppenheimer;
+
+    @BeforeEach
+    void initData() {
+        movieOppenheimer = Movie.builder()
+                .title("Oppenheimer")
+                .year((short) 2023)
+                .build();
+        persons = List.of(
+                Person.builder()
+                        .name("Christopher Nolan")
+                        .build(),
+                Person.builder()
+                        .name("Cillian Murphy")
+                        .build(),
+                Person.builder()
+                        .name("Emily Blunt")
+                        .build(),
+                Person.builder()
+                        .name("Matt Damon")
+                        .build()
+        );
+        entityManager.persist(movieOppenheimer);
+        persons.forEach(entityManager::persist);
+        entityManager.flush(); // SQL insert
+        entityManager.clear(); // empty hibernate cache
+    }
 
     @Test
     void testSave(){
@@ -29,4 +71,43 @@ class MovieRepositoryTest {
         System.out.println(movie);
     }
 
+    // save 1 movie
+    // save persons: directors + 3 actors
+    // update movie to set director and actors
+
+    @Test
+    void testSetDirector(){
+        var directorId = persons.get(0).getId();
+        var movieId = movieOppenheimer.getId();
+        // fetch movie and director from DB
+        var director = entityManager.find(Person.class, directorId);
+        var movie = entityManager.find(Movie.class, movieId);
+
+        // update movie with its director
+        movie.setDirector(director);
+        movieRepository.flush(); // SQL: update
+
+        // TODO: verify
+    }
+
+    @Test
+    void testSetActors() {
+        var movieId = movieOppenheimer.getId();
+        var actorIds = persons.stream()
+                .skip(1)
+                .map(Person::getId)
+                .toList();
+        // fetch data from DB
+        var movie = entityManager.find(Movie.class, movieId);
+        var actors = actorIds.stream()
+                .map(id -> entityManager.find(Person.class, id))
+                .toList();
+
+        // update movie with its actors
+        movie.getActors().addAll(actors);
+        movieRepository.flush(); // SQL: 'update' table play
+
+        // TODO: verify
+
+    }
 }
