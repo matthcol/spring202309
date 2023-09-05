96a27cecdbcc765858a423ffecaff287d1bbe465
matthcol <matthias.colin@gmail.com>
2023-09-05 13:30:59 +0200

    ResponseEntity example

--- src/main/java/org/example/movieapi/controller/DummyController.java
+++ src/main/java/org/example/movieapi/controller/DummyController.java
@@ -1,7 +1,9 @@
 package org.example.movieapi.controller;
 
 import org.example.movieapi.entity.Movie;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.awt.*;
@@ -20,6 +22,7 @@ public class DummyController {
             consumes = MediaType.APPLICATION_JSON_VALUE,  // default
             produces = MediaType.APPLICATION_JSON_VALUE   // default
     )
+    @ResponseStatus(HttpStatus.CREATED)
     public Movie add(@RequestBody Movie movie){
         movie.setId(12345);
         return movie;
@@ -51,6 +54,14 @@ public class DummyController {
                 .title(title)
                 .year(year)
                 .build();
+    }
+
+    @GetMapping("custom")
+    public ResponseEntity<Movie> custom(){
+        return ResponseEntity.status(HttpStatus.I_AM_A_TEAPOT)
+                .body(
+                        Movie.builder().title("Teapot").build()
+                );
     }
 
 }
