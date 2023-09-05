6dca5f647dc52ecf117cc05ed405ac10ee7f70c1
matthcol <matthias.colin@gmail.com>
2023-09-05 09:58:34 +0200

    jpa repository with method auto query

--- src/main/java/org/example/movieapi/repository/MovieRepository.java
+++ src/main/java/org/example/movieapi/repository/MovieRepository.java
@@ -1,8 +1,27 @@
 package org.example.movieapi.repository;
 
 import org.example.movieapi.entity.Movie;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Stream;
+
 public interface MovieRepository extends JpaRepository<Movie, Integer> {
+
+    // Spring vocabulary for method with query auto
+    //  https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#jpa.query-methods.query-creation
+    // Results can be: Set, List, Stream, Optional
+    Set<Movie> findByTitleContainingIgnoreCase(String title);
+
+    // by year between
+    Stream<Movie> findByYearBetween(short year1, short year2, Sort sort);
+
+    // filmography as an actor
+    List<Movie> findByActorsNameEndingWithIgnoreCaseOrderByYear(String partName);
+
+    // filmography as a director
+    List<Movie> findByDirectorNameEndingWithIgnoreCaseOrderByYearDesc(String partName);
 
 }
--- src/test/java/org/example/movieapi/repository/MovieRepositoryQueries.java
+++ src/test/java/org/example/movieapi/repository/MovieRepositoryQueries.java
@@ -4,11 +4,13 @@ import org.example.movieapi.entity.Movie;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.util.stream.Stream;
@@ -70,4 +72,31 @@ public class MovieRepositoryQueries {
         var movies = movieRepository.findAll(movieExample);
         movies.forEach(System.out::println);
     }
+
+    @Test
+    void findByTitle() {
+        movieRepository.findByTitleContainingIgnoreCase("star")
+                .forEach(System.out::println);
+    }
+
+    @Test
+    void findByYearBetween(){
+        movieRepository.findByYearBetween((short) 1982, (short) 1984, Sort.by("year", "title"))
+                .forEach(System.out::println);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings={"eastwood", "tarantino", "steve mcqueen"})
+    void findByDirector(String partName){
+        movieRepository.findByDirectorNameEndingWithIgnoreCaseOrderByYearDesc(partName)
+                .forEach(System.out::println);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings={"eastwood", "tarantino", "steve mcqueen"})
+    void findByActor(String partName){
+        movieRepository.findByActorsNameEndingWithIgnoreCaseOrderByYear(partName)
+                .forEach(System.out::println);
+    }
+
 }
