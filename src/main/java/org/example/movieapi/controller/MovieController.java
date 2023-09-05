3df841058730a3bc1d65ef6dbe12c76376de4397
matthcol <matthias.colin@gmail.com>
2023-09-05 15:42:46 +0200

    handle not found in controller

--- src/main/java/org/example/movieapi/controller/MovieController.java
+++ src/main/java/org/example/movieapi/controller/MovieController.java
@@ -4,10 +4,14 @@ import org.example.movieapi.dto.MovieDetail;
 import org.example.movieapi.dto.MovieSimple;
 import org.example.movieapi.service.MovieService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.text.MessageFormat;
 
 @RestController
 @RequestMapping("/api/movie")
@@ -19,6 +23,9 @@ public class MovieController {
     @GetMapping("{id}")
     public MovieDetail getById(@PathVariable("id") int id){
         return movieService.getById(id)
-                .get(); // provisoire
+                .orElseThrow(() -> new ResponseStatusException(
+                        HttpStatus.NOT_FOUND,
+                        MessageFormat.format("Movie not found with id <{0}>", id)
+                ));
     }
 }
