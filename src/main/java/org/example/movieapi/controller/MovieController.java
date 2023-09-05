271762f6ea70420985fc5477ec53f4959a91524e
matthcol <matthias.colin@gmail.com>
2023-09-05 14:58:29 +0200

    test controller

--- src/main/java/org/example/movieapi/controller/MovieController.java
+++ src/main/java/org/example/movieapi/controller/MovieController.java
@@ -2,6 +2,8 @@ package org.example.movieapi.controller;
 
 import org.example.movieapi.dto.MovieDetail;
 import org.example.movieapi.dto.MovieSimple;
+import org.example.movieapi.service.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/movie")
 public class MovieController {
+
+    @Autowired
+    private MovieService movieService;
 
     @GetMapping("{id}")
     public MovieDetail getById(@PathVariable("id") int id){
---
+++ src/test/java/org/example/movieapi/controller/MovieControllerTest.java
@@ -0,0 +1,67 @@
+package org.example.movieapi.controller;
+
+import org.example.movieapi.dto.MovieDetail;
+import org.example.movieapi.entity.Movie;
+import org.example.movieapi.service.MovieService;
+import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@WebMvcTest(controllers = MovieController.class)
+class MovieControllerTest {
+
+    final static String BASE_URL = "/api/movie";
+
+    // HTTP requests
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    MovieService movieService;
+
+    @Test
+    void getById_whenFound() throws Exception {
+        int id = 123;
+        String title = "Barbie";
+        short year = 2023;
+        // prepare mock service response
+        var optMovie = Optional.<MovieDetail>of(
+                MovieDetail.builder()
+                        .id(id)
+                        .title(title)
+                        .year(year)
+                        // add duration, actors, director
+                        .build()
+        );
+        BDDMockito.given(movieService.getById(id))
+                .willReturn(optMovie);
+        // call controller
+        String url = BASE_URL + "/{id}";
+        mockMvc.perform(MockMvcRequestBuilders.get(url, id)
+                .accept(MediaType.APPLICATION_JSON)
+        ).andDo(MockMvcResultHandlers.print());
+
+        // verify response
+
+        // verify mock service has been called
+        BDDMockito.then(movieService)
+                .should()
+                .getById(id);
+    }
+
+    @Test
+    void getById_whenNotFound() {
+
+    }
+
+}
